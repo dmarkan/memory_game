f08d0e442c24d7ef71f21e4703b2eb12a234fe6e
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-05-22 15:11:17 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,169 @@
+let back_1 = document.querySelector(".back_1");
+let back_2 = document.querySelector(".back_2");
+let back_3 = document.querySelector(".back_3");
+let back_4 = document.querySelector(".back_4");
+let back_5 = document.querySelector(".back_5");
+let back_6 = document.querySelector(".back_6");
+let back_7 = document.querySelector(".back_7");
+let back_8 = document.querySelector(".back_8");
+let back_9 = document.querySelector(".back_9");
+let back_10 = document.querySelector(".back_10");
+let back_11 = document.querySelector(".back_11");
+let back_12 = document.querySelector(".back_12");
+
+back_1.addEventListener("click", function () {
+    back_1.style.visibility = "hidden";
+    to1 = setTimeout(function () {
+        back_1.style.visibility = "visible";
+    }, 1000);
+    if ((back_1.style.visibility == "hidden") && (back_8.style.visibility == "hidden")) {
+        clearTimeout(to1);
+        clearTimeout(to2);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_2.addEventListener("click", function () {
+    back_2.style.visibility = "hidden";
+    to3 = setTimeout(function () {
+        back_2.style.visibility = "visible";
+    }, 1000);
+    if ((back_2.style.visibility == "hidden") && (back_7.style.visibility == "hidden")) {
+        clearTimeout(to3);
+        clearTimeout(to4);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_3.addEventListener("click", function () {
+    back_3.style.visibility = "hidden";
+    to5 = setTimeout(function () {
+        back_3.style.visibility = "visible";
+    }, 1000);
+    if ((back_3.style.visibility == "hidden") && (back_4.style.visibility == "hidden")) {
+        clearTimeout(to5);
+        clearTimeout(to6);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_4.addEventListener("click", function () {
+    back_4.style.visibility = "hidden";
+    to6 = setTimeout(function () {
+        back_4.style.visibility = "visible";
+    }, 1000);
+    if ((back_3.style.visibility == "hidden") && (back_4.style.visibility == "hidden")) {
+        clearTimeout(to5);
+        clearTimeout(to6);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_5.addEventListener("click", function () {
+    back_5.style.visibility = "hidden";
+    to7 = setTimeout(function () {
+        back_5.style.visibility = "visible";
+    }, 1000);
+    if ((back_5.style.visibility == "hidden") && (back_6.style.visibility == "hidden")) {
+        clearTimeout(to7);
+        clearTimeout(to8);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_6.addEventListener("click", function () {
+    back_6.style.visibility = "hidden";
+    to8 = setTimeout(function () {
+        back_6.style.visibility = "visible";
+    }, 1000);
+    if ((back_5.style.visibility == "hidden") && (back_6.style.visibility == "hidden")) {
+        clearTimeout(to7);
+        clearTimeout(to8);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_7.addEventListener("click", function () {
+    back_7.style.visibility = "hidden";
+    to4 = setTimeout(function () {
+        back_7.style.visibility = "visible";
+    }, 1000);
+    if ((back_2.style.visibility == "hidden") && (back_7.style.visibility == "hidden")) {
+        clearTimeout(to3);
+        clearTimeout(to4);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_8.addEventListener("click", function () {
+    back_8.style.visibility = "hidden";
+    to2 = setTimeout(function () {
+        back_8.style.visibility = "visible";
+    }, 1000);
+    if ((back_1.style.visibility == "hidden") && (back_8.style.visibility == "hidden")) {
+        clearTimeout(to1);
+        clearTimeout(to2);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_9.addEventListener("click", function () {
+    back_9.style.visibility = "hidden";
+    to9 = setTimeout(function () {
+        back_9.style.visibility = "visible";
+    }, 1000);
+    if ((back_9.style.visibility == "hidden") && (back_11.style.visibility == "hidden")) {
+        clearTimeout(to9);
+        clearTimeout(to10);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_10.addEventListener("click", function () {
+    back_10.style.visibility = "hidden";
+    to11 = setTimeout(function () {
+        back_10.style.visibility = "visible";
+    }, 1000);
+    if ((back_10.style.visibility == "hidden") && (back_12.style.visibility == "hidden")) {
+        clearTimeout(to11);
+        clearTimeout(to12);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_11.addEventListener("click", function () {
+    back_11.style.visibility = "hidden";
+    to10 = setTimeout(function () {
+        back_11.style.visibility = "visible";
+    }, 1000);
+    if ((back_9.style.visibility == "hidden") && (back_11.style.visibility == "hidden")) {
+        clearTimeout(to9);
+        clearTimeout(to10);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
+back_12.addEventListener("click", function () {
+    back_12.style.visibility = "hidden";
+    to12 = setTimeout(function () {
+        back_12.style.visibility = "visible";
+    }, 1000);
+    if ((back_10.style.visibility == "hidden") && (back_12.style.visibility == "hidden")) {
+        clearTimeout(to11);
+        clearTimeout(to12);
+    }
+    if (back_1.style.visibility == "hidden" && back_2.style.visibility == "hidden" && back_3.style.visibility == "hidden" && back_4.style.visibility == "hidden" && back_5.style.visibility == "hidden" && back_6.style.visibility == "hidden" && back_7.style.visibility == "hidden" && back_8.style.visibility == "hidden" && back_9.style.visibility == "hidden" && back_10.style.visibility == "hidden" && back_11.style.visibility == "hidden" && back_12.style.visibility == "hidden") {
+        alert("Čestitam, pronašao si sve parove!")
+    }
+})
